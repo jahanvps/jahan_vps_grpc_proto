0e923275aea1b9fdbeeaea8b98cedf662671baef
jahanvps <jahanvps@gmail.com>
2024-09-14 16:57:17 +0330

    new

--- task.js
+++ task.js
@@ -1,23 +1,11 @@
 import cron from 'node-cron';
+import { simpleGit, CleanOptions } from 'simple-git';
 
+const git = simpleGit().clean(CleanOptions.FORCE);
 
-function doGitPull() {
-    // CREATE GIT PULL CHILD PROCESS
-    const pull = cp.spawn('git',['pull'],{cwd:gitDir}) 
-    // HANDLE CHILD PROCESS' OUTPUT
-    pull.stdout.on('data', d => console.log(d.toString()))
-    // HANDLE CHILD PROCESS' ERROR OUTPUT
-    pull.stderr.on('data', e => console.error(e.toString()))
-    // HANDLE CHILD PROCESS ERRORS
-    pull.on('error', e=> res.sendStatus(500))
-    // HANDLE PROCESS EXIT CODES. IF 0, SUCCESSFULLY CONTINUE
-    pull.on('exit',code => {
-      if (code == 0) next()
-      else res.sendStatus(500)
-    })
-  }
   console.log('task puller!! started');
-cron.schedule('*/1 * * * *', () => {
+cron.schedule('*/1 * * * *', async () => {
   console.log('running a task every minute');
-  doGitPull();
+  const res = await git.pull();
+  console.log(res);
 });
